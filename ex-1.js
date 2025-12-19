7bd3d558b5505621cca8d7a87d6514dbf48fdf92
Dragon <parit.menklay@gmail.com>
2025-12-19 16:49:59 +0700

    feat: update word processing functions to return correct lengths and filter by character count

--- ex-1.js
+++ ex-1.js
@@ -2,7 +2,7 @@ let words = ["hello", "world"];
 
 function getWordLengths(words) {
   // Start coding here
-  return words.map(() => 5);
+  return words.map((word) => word.length);
 }
 
 const result = getWordLengths(words);
